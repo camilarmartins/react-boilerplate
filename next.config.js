52d87010fd1b5cbb762381ed73f87d162df1bf04
camilarmartins <camilamarttins20@gmail.com>
2022-10-12 22:16:55 -0400

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = nextConfig
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+})
+
+module.exports = withPWA({
+  swcMinify: true,
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  }
+})
